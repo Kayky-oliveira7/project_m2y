3075bdbad418c6bc2bee134e3b7a6e72c0bbfbb9
Kayky Oliveira <kaykycalhas@gmial.com>
2022-08-09 17:18:26 -0300

    -Na home page foi adicionado um container e o componente MovieListWidget

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_m2y/features/components/movie_lits/movie_list_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 350,
+              color: Colors.blue,
+            ),
+            const MovieListWidget(),
+          ],
+        ),
+      ),
+    );
   }
 }

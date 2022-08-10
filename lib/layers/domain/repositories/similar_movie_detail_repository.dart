f5bc3c263ed9df69add9d49a3d2e43c1d61eaccf
Kayky Oliveira <kaykycalhas@gmial.com>
2022-08-10 18:08:01 -0300

    -Foi feita a alteraçao de nomeclatura do metodo repository

--- lib/layers/domain/repositories/similar_movie_detail_repository.dart
+++ lib/layers/domain/repositories/similar_movie_detail_repository.dart
@@ -1,5 +1,5 @@
 import 'package:project_m2y/layers/domain/entitys/similar_movie_detail_entity.dart';
 
 abstract class SimililarMovieDetailRepository {
-  Future<SimilarMovieDetailModel> getSimilarMovieDetail(Result result);
+  Future<SimilarMovieDetailEntity> getSimilarMovieDetail(Result result);
 }

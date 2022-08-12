d693f3c5faf327db43b7a942eca2f6204ddfb2ef
Kayky Oliveira <kaykycalhas@gmial.com>
2022-08-12 00:27:26 -0300

    -Foi alterado o import do use case

--- lib/layers/domain/usecases/get_similar_movie_detail_use_case_impl.dart
+++ lib/layers/domain/usecases/get_similar_movie_detail_use_case_impl.dart
@@ -1,4 +1,4 @@
-import 'package:project_m2y/features/comom/use_case/use_case.dart';
+import 'package:project_m2y/comom/use_case/use_case.dart';
 import 'package:project_m2y/layers/domain/entitys/similar_movie_detail_entity.dart';
 import 'package:project_m2y/layers/domain/repositories/similar_movie_detail_repository.dart';
 

cded516538d76c4fb952a6c47896ffa6bf13d176
Kayky Oliveira <kaykycalhas@gmial.com>
2022-08-12 16:16:06 -0300

    -Fo feita algumas ateraçoes na home page

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:project_m2y/features/components/movie-detail/movie_detail_widget.dart';
 import 'package:project_m2y/features/components/movie_lits/movie_list_widget.dart';
 import 'package:project_m2y/layers/data/data_sources/similar_movie_detail_data_source.dart';
 import 'package:project_m2y/layers/data/models/similar_movie_detail_model.dart';
@@ -26,15 +25,10 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: const [
-            MovieDetailWidget(),
-            MovieListWidget(),
-          ],
-        ),
+        child: MovieListWidget(),
       ),
     );
   }

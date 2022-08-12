ce475be2e85fcd9c7b3c47ce881df25884710500
Kayky Oliveira <kaykycalhas@gmial.com>
2022-08-12 14:29:13 -0300

    -Foram feitas algumas alteraçaes na camda de data

--- lib/layers/data/repositories/similar_movie_detail_repository_impl.dart
+++ lib/layers/data/repositories/similar_movie_detail_repository_impl.dart
@@ -1,5 +1,7 @@
 import 'package:project_m2y/layers/data/data_sources/similar_movie_detail_data_source.dart';
+import 'package:project_m2y/layers/data/models/similar_movie_detail_model.dart';
 import 'package:project_m2y/layers/domain/entitys/similar_movie_detail_entity.dart';
+import 'package:project_m2y/layers/domain/mappers/similar_movie_detail_model_to_entity_mapper.dart';
 import 'package:project_m2y/layers/domain/repositories/similar_movie_detail_repository.dart';
 
 class SimilarMovieDetailRepositoryImpl
@@ -8,9 +10,14 @@ class SimilarMovieDetailRepositoryImpl
 
   SimilarMovieDetailRepositoryImpl(this._similarMovieDetailDataSource);
   @override
-  Future<List<Result>> getSimilarMovieDetail(
+  Future<List<ResultEntity>> getSimilarMovieDetail(
       SimilarMovieDetailEntity similarMovieDetailEntity) {
-    // TODO: implement getSimilarMovieDetail
-    throw UnimplementedError();
+    return _similarMovieDetailDataSource
+        .getSimilarMovieDetail(SimilarMovieDetailModel())
+        .then((value) {
+      return value
+          .map((e) => SimilarMovieDetailModelToEntityMapper(e).toEntity())
+          .toList();
+    });
   }
 }
